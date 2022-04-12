8197b0f1b78c5ed81b1a79aafb238f769d6626cd
fadiACG <Fadi.Nuqul@acg.edu>
2022-04-12 22:45:45 +0300

    new

--- padi_final/settings.py
+++ padi_final/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'padi_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'padi_app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
